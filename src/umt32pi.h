22dd047c860de6958c7fd149bb7660c86b207008
tantanGH <tantanGH>
2025-02-11 22:30:58 +0900

    changed gsvol sysex

--- src/umt32pi.h
+++ src/umt32pi.h
@@ -1,6 +1,6 @@
 #ifndef __H_UMT32PI__
 
-#define PROGRAM_VERSION  "0.3.1 (2025/02/11)"
+#define PROGRAM_VERSION  "0.3.2 (2025/02/11)"
 
 #define INTERFACE_MIDI_BOARD    (0)
 #define INTERFACE_RS_MIDI       (1)
